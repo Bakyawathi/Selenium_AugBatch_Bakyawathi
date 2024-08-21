7bab34eb24f7f2d23ec56514ff8fb1971e88510d
Bakyawathi <bakyawathir@gmail.com>
2024-08-21 18:37:23 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear= 2008;
+		char shortcutKey = 'C';
+		
+		
+		System.out.println("Broswer version is "+browserVersion);
+		System.out.println("Broswer Name is "+developer);
+
+		System.out.println("Is the browser beta version "+isBeta  );
+
+		System.out.println("Release  Year is "+releaseYear);
+
+		System.out.println("Shortcut key is "+shortcutKey);
+
+	}
+
+}
